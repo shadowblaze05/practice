52309807978caefe35725adb682ae66955218347
shadowblaze05 <shadowblaze259@gmail.com>
2025-09-25 20:29:08 +0800

    more features

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+        'title': 'Home Page',
+        'features': [
+            'Django', 
+            'Templates', 
+            'Static Files'
+            ]
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About Page'})
+
+def hello(request, name):
+    return render(request, 'hello.html', {'name': name})
