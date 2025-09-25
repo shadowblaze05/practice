4a03cd09ac76e99efc4d46384971aa33a3d047e5
shadowblaze05 <shadowblaze259@gmail.com>
2025-09-25 21:22:41 +0800

    added error 404 and 500

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3u@fy!agejsjlfy4ch+mjv)b980ktouv-rd8cv)(z&&4qne)wp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- pages/views.py
+++ pages/views.py
@@ -25,3 +25,9 @@ def gallery(request):
         'img3.jpg'
     ]
     return render(request, 'gallery.html', {'images': images})
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
+
+def server_error_view(request):
+    return render(request, '500.html', status=500)  
